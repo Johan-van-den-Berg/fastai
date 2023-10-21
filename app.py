a113a4259ac1a5250d2568d3e99542dec8e61e69
Johan van den Berg <92765513+Johan-van-den-Berg@users.noreply.github.com>
2023-10-21 18:31:45 +0200

    odified:   app.py

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!!"
 
-iface=gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+gr.Interface(fn=greet, inputs="text", outputs="text").launch()
